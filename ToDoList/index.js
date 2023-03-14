9b23322e4d8eef2d50c8c1c620aecc5c3516b502
Dan Mundy <danielmundy160@gmail.com>
2023-03-13 21:38:12 -0400

    Test change
    Testing change

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -28,7 +28,11 @@ function App(){
     <div className="app">
       <div className="todo-list" >
         {todos.map((todo, i) => (
-          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}>
+          <BTN key={i} index={i} todo={todo} remove={removeTodo}/>
+          </Todo>
+          
+          
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
